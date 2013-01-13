69576aa62c3c5740321e69068bbd2f582f8bb401
Szuuuken <daniel@szuuuken.at>
2013-01-13 12:33:09 +0100

    stuff

--- controllers/track/index.js
+++ controllers/track/index.js
@@ -1,11 +1,12 @@
 var auth = require('../../lib/auth/auth');
 var music = require('../../lib/music/music');
-//var fs = require('fs');
+var fs = require('fs');
 //var path = require('path');
 //var mime = require('mime');
 //var sys = require('sys');
 var filed = require('filed');
 //var oppressor = require('oppressor');
+var util = require('util');
 
 var mimeTypes = {
     "html": "text/html",
@@ -23,25 +24,50 @@ exports.show = function(req, res){
 		console.log(track.path);
  		var filename = track.path;
  		
-		/*var readStream = fs.createReadStream(filename);
+		var response = res;
+		var fileSystem = fs;
+		var filePath = track.path;//path.join(__dirname, 'AstronomyCast Ep. 216 - Archaeoastronomy.mp3');
 
-  		readStream.on('open', function () {
-    			readStream.pipe(res);
-  		});
-	
-		readStream.on('error', function(err) {
-    			res.end(err);
-  		});*/
-	/*	
-	sys.pump(fs.createReadStream(filename, {
-  'bufferSize': 4 * 1024
-}), res)*/
+		/*var stat = fileSystem.statSync(filePath);
+    
+    		response.writeHead(200, {
+        		'Content-Type': 'audio/mpeg', 
+        		'Content-Length': stat.size
+    		});
+    
+    		var readStream = fileSystem.createReadStream(filePath);
+    		readStream.on('data', function(data) {
+       	 		var flushed = response.write(data);
+        		// Pause the read stream when the write stream gets saturated
+        		if(!flushed)
+            			readStream.pause();
+    		});
+    
+    		response.on('drain', function() {
+        		// Resume the read stream when the write stream gets hungry 
+        		readStream.resume();    
+    		});
+    
+    		readStream.on('end', function() {
+        		response.end();        
+    		});*/
 
+/*		var stat = fileSystem.statSync(filePath);
+    
+    response.writeHead(200, {
+        'Content-Type': 'audio/mpeg', 
+        'Content-Length': stat.size
+    });
+    
+    var readStream = fileSystem.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to util.pump()
+    util.pump(readStream, response);
+*/
 
 		//filed(filename).pipe(oppressor(req)).pipe(res);
-		req.pipe(filed(filename)).pipe(res);
+		//req.pipe(filed(filename)).pipe(res);
 
-		//res.sendfile(track.path);
+		res.sendfile(track.path);
 	});
   });
 };
