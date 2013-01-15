86a8473e8b20df8a48499ee07fde4139ebde9450
Szuuuken <daniel@szuuuken.at>
2013-01-15 17:28:22 +0100

    dependencies cleanup

--- controllers/track/index.js
+++ controllers/track/index.js
@@ -1,20 +1,6 @@
 var auth = require('../../lib/auth/auth');
 var music = require('../../lib/music/music');
 var fs = require('fs');
-//var path = require('path');
-//var mime = require('mime');
-//var sys = require('sys');
-var filed = require('filed');
-//var oppressor = require('oppressor');
-var util = require('util');
-
-var mimeTypes = {
-    "html": "text/html",
-    "jpeg": "image/jpeg",
-    "jpg": "image/jpeg",
-    "png": "image/png",
-    "js": "text/javascript",
-    "css": "text/css"};
 
 exports.show = function(req, res){
   auth.restrict(req,res,function(){
