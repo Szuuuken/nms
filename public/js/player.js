e9f3a4e7e456d5468ea7bc31a35e7e693c0a82ef
Szuuuken <daniel@szuuuken.at>
2013-01-22 21:51:05 +0100

    first filterView version finished; added TrackTable

--- public/js/player.js
+++ public/js/player.js
@@ -1,9 +1,14 @@
 var as;
 var audio;
+
 var genres;
 var artists;
 var ablums;
 var tracks;
+
+var genreFilter = -1;
+var artistFilter = -1;
+var albumFilter = -1;
 
 function init(){
 	resize();
@@ -11,36 +16,101 @@ function init(){
 	loadInformations();
 }
 
+function setGenreFilter(filter){
+	genreFilter = filter;
+	setArtistFilter(-1);
+}
+
+function setArtistFilter(filter){
+	artistFilter = filter;
+	setAlbumFilter(-1);
+}
+
+function setAlbumFilter(filter){
+	albumFilter = filter;
+}
+
+function filter(){
+	var filteredArtists = artists.filter(function(index){
+		if(genreFilter == -1) return index;
+		if($.inArray(genreFilter,index.genres) > -1){
+			return index;
+		}
+	});
+	
+	var filteredAlbums = albums.filter(function(index){
+		var filterGenre = ($.inArray(genreFilter,index.genres) > -1) || (genreFilter == -1);
+		var filterArtist = (artistFilter == index.artistId) || (artistFilter == -1);
+		if( filterGenre && filterArtist){
+			return index;
+		}
+	});
+
+	var filteredTracks = tracks.filter(function(index){
+		var filterGenre = (genreFilter == index.genre.id) || (genreFilter == -1);
+		var filterArtist = (artistFilter == index.artist.id) || (artistFilter == -1);
+		var filterAlbum = (albumFilter == index.album.id) || (albumFilter == -1);
+		if( filterGenre && filterArtist && filterAlbum){
+			return index;
+		}
+	});
+
+	return {artists:filteredArtists,albums:filteredAlbums,tracks:filteredTracks};
+}
+
+function rebuiltUIFilterBox(filteredItems, filterBoxId){
+	var filterBox = $(filterBoxId);
+	filterBox.children().remove();
+
+	filterBox.append(new Option("All", -1));
+	$.each(filteredItems, function(index, value) {
+		filterBox.append(new Option(value.name, value.id));
+	});
+}
+
+function rebuiltUIFilterBoxesByGenre(){
+	var filteredItems = filter();
+	rebuiltUIFilterBox(filteredItems.artists,"#artists");
+	rebuiltUIFilterBox(filteredItems.albums,"#albums");
+}
+
+function rebuiltUIFilterBoxesByArtist(){
+	var filteredItems = filter();
+	rebuiltUIFilterBox(filteredItems.albums,"#albums");
+}
+
+function filterTrackTable(){
+	var trackTable = $('#songs');
+	var filteredItems = filter();
+
+	trackTable.children().find( 'tr:not(:first)' ).remove();
+	$.each(filteredItems.tracks,function(index,value){
+		trackTable.append("<tr><td>"+value.name+"</td><td>"+value.artist.name+"</td><td>"+getAddButton(value)+"</td></tr>");	
+	});
+}
+
+function getAddButton(track){
+	return "<button id=\""+track.sha1+"\" onClick=\"addTrackToPlayer(this,'"+track.name+" - "+track.artist.name+"')\">Add</button>";
+}
 function initEvents(){
 	$('#genres').change(function(){
 		var genreId = parseInt($(this).val());
-	
-		var filterdArtists = artists.filter(function(index){
-			if($.inArray(genreId,index.genres) > -1){
-				return index;
-			}
-		});
-		
-		var filterdAlbums = albums.filter(function(index){
-			if($.inArray(genreId,index.genres) > -1){
-				return index;
-			}
-		});
-
-		var filterdTracks = tracks.filter(function(index){
-			if($.inArray(genreId,index.genres) > -1){
-				return index;
-			}
-		});
+		setGenreFilter(genreId);
+		rebuiltUIFilterBoxesByGenre();
+		filterTrackTable();
 	});
 
 	$('#artists').change(function(){
-		//console.log($(this).val());
-		$(this).children().eq(this.selectedIndex).appendTo('#hidden');
+		var artistId = parseInt($(this).val());
+		setArtistFilter(artistId);
+		rebuiltUIFilterBoxesByArtist();		
+		filterTrackTable();
 	}); 
 
 	$('#albums').change(function(){
-		console.log($(this).val());
+		var albumId = parseInt($(this).val());
+		setAlbummFilter(albumId);
+		filterTrackTable();
 	}); 
 }
 
